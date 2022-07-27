58210dcb951bc766133ab71829f9c2476a8eda87
Aashil <aashil556@gmail.com>
2022-07-27 19:46:50 +0530

    gud2

--- main.go
+++ main.go
@@ -25,6 +25,20 @@ func main() {
 	// if err != nil {
 	// 	log.Fatalf("cannot start server : %v", err)
 	// }
+	// conn, err := grpc.Dial("0.0.0.0:80", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// if err != nil {
+	// 	log.Fatalf("cannot dial to the server : %v", err)
+	// }
+	// client := pb.NewTimeZoneServiceClient(conn)
+	// req := &pb.TimeZoneRequest{
+	// 	Latitude:  43.8287724,
+	// 	Longitude: -79.542908,
+	// }
+	// res, err := client.TimeZoneDetails(context.Background(), req)
+	// if err != nil {
+	// 	log.Fatalf("cannot recieve response from the server: %v", err)
+	// }
+	// fmt.Printf("response :\n utcOffSet: %v\n Zonename: %v\n Time In That Zone: %v\n", res.UtcOffset, res.ZoneName, res.TimeInThatZone)
 	mux := runtime.NewServeMux(runtime.WithMetadata(func(ctx context.Context, req *http.Request) metadata.MD {
 		return metadata.Pairs("client-ip", req.RemoteAddr, "client-ip-header", req.Header.Get("x-forwarded-for"))
 	}))
